b422a3ba027ff918613894dc02c5752a56beafc0
J Tsai <jtsai972@gmail.com>
2019-10-13 20:29:42 -0500

    Set up the spotify and movie stuff

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -8,11 +8,11 @@ const axios = require("axios");
 const moment = require('moment');
 
 //API
-const Spotify = require('node-spotify-api');
+const spotifyAPI = require('node-spotify-api');
 
 //keys
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify); //example from assignment
+var Spotify = new spotifyAPI(keys.spotify); //example from assignment
 
 //formatting variables
 var cmdTxt = "Command: node run.js ";
@@ -20,17 +20,17 @@ var cmdTxt = "Command: node run.js ";
 var breakEquals = "==========================\n",
     breakDashes = "--------------------------\n";
 
-var concertFn = function concert(search, name){
-  var queryURL = `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`;
+var concert = function(name){
+  var queryURL = `https://rest.bandsintown.com/artists/${name}/events?app_id=codingbootcamp`;
 
-  var cmd = cmdTxt + "concert-this " + name;
+  var cmd = cmdTxt + "concert-this " + name + "\n\n";
 
   var starterText = 
-    breakEquals +
-      "Searching for concerts!\n" + 
-    breakEquals +"\n"+ 
-      "Upcoming Events\n" + 
-    breakDashes;
+    `${breakEquals} 
+      Searching for concerts!\n 
+    ${breakEquals}\n
+      Upcoming Events\n
+    ${breakDashes}`;
 
   console.log(starterText);
   var printArr = [cmd, starterText];
@@ -52,34 +52,89 @@ var concertFn = function concert(search, name){
       }
     }
   ).then(function() {
-    log(printArr);
+    //log(printArr);
     console.log(printArr);
   });
   
 }
 
-var spotifyFn = function spotify(){
-  console.log("Retrieving song details!");
+var spotifyFn = function(name){
+  let starterText = 
+    breakEquals + 
+      "Retrieving song details!" + 
+    breakEquals + 
+      "\n";
+
+  console.log(starterText);
+  log(starterText);
+
+  Spotify
+    .search({ type: 'track', query: name })
+    .then(function(response) {
+      //console.log(response.tracks);
+      let track = response.tracks.items[0];
+      let album = track.album;
+      //console.log(track);
+
+      let printArr = [
+        breakDashes,
+        preview = "Preview link: " + album.external_urls.spotify,
+        artist = "Artist:       " + album.artists[0].name,
+        song = "Song:         " + track.name,
+        albumName = "Album:        " + album.name
+      ]
+      for(let i = 0; i < printArr.length; i++) {
+        console.log(printArr[i]);
+        log(printArr[i]);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-var movieFn = function movie(){
+var movie = function (search){
   console.log("Grabbing movie information");
+  var queryURL = `https://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`;
+
+  query(queryURL).then(
+    function(movie) {
+      console.log(movie);
+      console.log(breakDashes);
+      console.log(`Title: ${movie.Title}`);
+      console.log(`Release year: ${movie.Year}`);
+      console.log(`IMDB Rating: ${movie.Ratings[0].Value}`);
+      console.log(`Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`);
+      console.log(`Country: ${movie.Country}`);
+      console.log(`Language: ${movie.Language}`);
+      console.log(`Plot: ${movie.Plot}`);
+      console.log(`Actors: ${movie.Actors}`);
+    }
+  );
 }
 
-var whatItSaysFn = function whatItSays(){
+var whatItSays = function(){
   console.log("What does the random text say?");
+  fs.readFile(
+    "./assets/text/random.txt", 
+    "utf8", 
+    function(error, data) {
+      if (error) { return console.log(error); }
+      console.log(data);
+    }
+  );
 }
 
 //Querying
 function query(queryURL) {
   return axios 
-    .get(queryURL) 
-    .then(
+    .get(queryURL) //Query
+    .then( //After query is made
       function(response) {
         // console.log(response.data);
         return response.data;
       })
-    .catch(function(error) {
+    .catch(function(error) { //If Query fails
       if (error.response) {
         console.log("---------------Data---------------");
         console.log(error.response.data);
@@ -105,22 +160,20 @@ function log(strArr){
   for( let i = 0; i < strArr.length+1; i++ ) {
     //making sure this is in order
     if(i < strArr.length) {
-      fs.appendFile(logLoc, strArr[i], function(error) {
+      fs.appendFileSync(logLoc, strArr[i], function(error) {
         if(error){console.log(error);};
       });
     } else {
-      fs.appendFile(logLoc, "\n\n" + breakEquals + "\n\n", function(error) {
+      fs.appendFileSync(logLoc, "\n\n" + breakEquals + "\n\n", function(error) {
         if(error){console.log(error);};
       });
     }
   }
-  
-  
 }
 
 
-//exports
-exports.concert = concertFn;
+//exporting stuff (to run.js)
+exports.concert = concert;
 exports.spotify = spotifyFn;
-exports.movie = movieFn;
-exports.whatItSays = whatItSaysFn;
+exports.movie = movie;
+exports.whatItSays = whatItSays;
